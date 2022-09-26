b3bc847441d460df3b49e6d4f3a3c22573af9989
Ivan Ustyantsev <vangold915@gmail.com>
2022-09-27 00:23:41 +0700

    example 012 edited method 4

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -32,14 +32,24 @@ int Method3() {
 // Console.WriteLine(year);
 
 // Вид 3. Методы, которые что-то возвращают и что-то принимают
+// string Method4(int count, string text) {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while(i < count) {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text) {
-    int i = 0;
     string result = String.Empty;
 
-    while(i < count) {
+    for (int i = 0; i < count; i++) {
         result = result + text;
-        i++;
     }
+
     return result;
 }
 
