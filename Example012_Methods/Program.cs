60d90c90e5a824e174998bc8034577f80e1991f8
Ivan Ustyantsev <vangold915@gmail.com>
2022-09-27 12:47:38 +0700

    example 012 added task about minmax algorithm

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -91,13 +91,50 @@ string Replace(string text, char oldValue, char newValue) {
     return result;
 }
 
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
 
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
 
-newText = Replace(newText, 'С', 'с');
-Console.WriteLine(newText);
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+
+// Упорядочивание данных внутри массива
+// Алгоритм сортировки методом выбора (методом минимакса, выбором максимального, выбором минимального)
+// Суть: имеется 6 8 3 2 1 4 5 7
+// 1. Найти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции
+// 3. Повторять пока есть не отсортированные элементы
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array) {
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++) {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) {
+    for (int i = 0; i < array.Length - 1; i++) {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++) {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
